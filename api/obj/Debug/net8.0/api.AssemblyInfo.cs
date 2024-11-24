ead85d5e6e8c9a47e0c286111cd154ea05abc374
DieselDE <vincentdomsta@gmail.com>
2024-11-24 13:13:20 +0100

    added comment getById

--- api/obj/Debug/net8.0/api.AssemblyInfo.cs
+++ api/obj/Debug/net8.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4730e177428b0aec178c70b69a02d7f1decc419f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+96733d4693ad805eb7ec3e552d26371d2b18f68b")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
